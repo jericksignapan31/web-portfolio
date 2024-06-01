1da00291365d5aa1839ab18ca7ac71158aff67de
Jerick Signapan <jericksignapan3331@gmail.com>
2024-06-01 17:38:46 +0800

    add pages component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path: 'pages',
+    component: PagesComponent,
+    title: 'Pages'
+  },
+  {
+    path: '',
+    redirectTo: '/pages',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
