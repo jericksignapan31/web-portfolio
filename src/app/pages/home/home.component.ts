90ffd549091af9179d0bb568f98094e80a75e9e3
Jerick Signapan <jericksignapan3331@gmail.com>
2024-06-05 17:27:24 +0800

    add animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   image = 'https://firebasestorage.googleapis.com/v0/b/mobi-pms.appspot.com/o/bgg.jpg?alt=media&token=da994c7f-43a1-4fdb-9c02-823fed715783'
+
+
+    loadImage() {
+    const imageDiv = document.querySelector('.image');
+    if (imageDiv) {
+      imageDiv.classList.add('loaded');
+    }
+  }
 }
