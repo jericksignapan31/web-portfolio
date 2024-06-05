59f9a737d1db965c0c46cad2338a94ae912b4e02
Jerick Signapan <jericksignapan3331@gmail.com>
2024-06-05 23:39:57 +0800

    added about

--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -4,11 +4,13 @@ import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { PageRoutingModule } from './page-routing.module';
 import { MaterialModule } from '../material-module';
+import { AboutComponent } from './about/about.component';
+import { CAComponent } from './ca/ca.component';
 
 
 
 @NgModule({
-  declarations: [PagesComponent, HomeComponent,],
+  declarations: [PagesComponent, HomeComponent, AboutComponent, CAComponent,],
   imports: [
     CommonModule,
     PageRoutingModule,
