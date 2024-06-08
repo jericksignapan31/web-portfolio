597e85db85807c932aec1488084903b3d0eef9a7
Jerick Signapan <jericksignapan3331@gmail.com>
2024-06-08 12:02:13 +0800

    update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit{
+   constructor(private el: ElementRef) { }
+
+  ngAfterViewInit() {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, observerOptions);
+
+    const experienceSection = this.el.nativeElement.querySelector('.experience');
+    observer.observe(experienceSection);
+  }
 
 }
--- src/app/pages/skill/skill.component.ts
+++ src/app/pages/skill/skill.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.css']
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit{
+ constructor(private el: ElementRef) { }
+
+  ngOnInit() {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, observerOptions);
+
+    const aboutSection = this.el.nativeElement.querySelector('.about');
+    const skillsSection = this.el.nativeElement.querySelector('.skills-container');
+
+    observer.observe(aboutSection);
+    observer.observe(skillsSection);
+  }
 
 }
